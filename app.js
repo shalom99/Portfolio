83da18259115c729ffd2075bcefd1576ed5f84bb
shalom99 <castroshalom77@gmail.com>
2022-12-31 00:22:51 +0800

    commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+// const SCPhoto = document.querySelector("#SCPhoto");
+// const overlay = document.querySelector("#overlay");
+// const borderOverlay = document.querySelector(".borderOverlay")
+// overlay.addEventListener("mouseenter", removeOverlay);
+// overlay.addEventListener("mouseleave", addOverlay);
+
+// function removeOverlay(){
+//     overlay.style.backgroundColor = "rgba(19, 162, 158, 0.0)";
+//     overlay.style.width = "350px";
+//     overlay.style.height = "350px";
+//     SCPhoto.style.width = "350px";
+//     SCPhoto.style.height = "350px";
+//     borderOverlay.style.width = "400px";
+//     borderOverlay.style.height = "400px";
+//     console.log("mouseenter")
+// }
+
+
+// function addOverlay(){
+//     overlay.style.backgroundColor = "rgba(19, 162, 158, 0.6)";
+//     SCPhoto.style.width = "300px";
+//     SCPhoto.style.height = "300px";
+//     overlay.style.width = "300px";
+//     overlay.style.height = "300px";
+//     borderOverlay.style.width = "300px";
+//     borderOverlay.style.height = "300px";
+//     console.log("mouseout")
+// }
