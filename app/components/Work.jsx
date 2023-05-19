694369b048b9db57d3a306f4bef9dfbd2d2cd57a
shalom99 <castroshalom77@gmail.com>
2023-05-19 19:26:30 +0800

    fixed projects text color

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -10,16 +10,16 @@ const Work = () => {
   return (
     <div name='work' className='w-full bg-[#1f2731]'>
       <div className='w-[95%] md:w-[90%] lg:w-[60%] xl:w-[50%] mx-auto py-5'> 
-      <h1 className='text-4xl font-semibold py-5  '>Projects</h1>
+      <h1 className='text-4xl font-semibold py-5 text-white  '>Projects</h1>
       <div className='flex flex-col gap-5'>
 
 
       <div className='grid grid-cols-12'>
           <div className='row-span-full col-start-2 md:col-start-6 col-end-12 md:col-end-13 col-span-7 flex flex-col justify-center gap-2 py-2 z-10'>
             <h3 className='md:text-end text-sm font-semibold text-primary'>Featured Project #1</h3>
-            <h2 className='text-2xl md:text-4xl font-bold md:text-end'>Pro-Lab</h2>
-            <p className='md:text-lg rounded-md   md:bg-gray-400 py-2 px-1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eum odit impedit autem quo quisquam quidem dolorum, temporibus enim similique nemo sapiente ratione nesciunt maiores vero. Sed natus nihil facere!</p>
-            <ul className='flex md:justify-end gap-2 mb-5'>
+            <h2 className='text-2xl md:text-4xl font-bold md:text-end text-white'>Pro-Lab</h2>
+            <p className='md:text-lg rounded-md text-white md:text-end md:bg-gray-400 py-2 px-1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eum odit impedit autem quo quisquam quidem dolorum, temporibus enim similique nemo sapiente ratione nesciunt maiores vero. Sed natus nihil facere!</p>
+            <ul className='flex md:justify-end gap-2 mb-5 text-white'>
                 <li className='border border-primary rounded-md p-2'>NextJS </li>
                 <li className='border border-primary rounded-md p-2'>Tailwind </li>
                 <li className='border border-primary rounded-md p-2'>JavaScript </li>
@@ -40,9 +40,9 @@ const Work = () => {
         <div className='grid grid-cols-12'>
           <div className='row-span-full col-start-2 md:col-start-1 col-end-13 md:col-end-8 col-span-7 flex flex-col justify-center gap-2 py-2 z-10'>
             <h3 className='text-sm font-semibold text-primary'>Featured Project #2</h3>
-            <h2 className='text-2xl md:text-4xl font-bold '>Pro-Lab</h2>
-            <p className='md:text-lg rounded-md  md:bg-gray-400 py-2 px-1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eum odit impedit autem quo quisquam quidem dolorum, temporibus enim similique nemo sapiente ratione nesciunt maiores vero. Sed natus nihil facere!</p>
-            <ul className='flex mb-5'>
+            <h2 className='text-2xl md:text-4xl text-white font-bold '>Pro-Lab</h2>
+            <p className='md:text-lg rounded-md text-white  md:bg-gray-400 py-2 px-1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eum odit impedit autem quo quisquam quidem dolorum, temporibus enim similique nemo sapiente ratione nesciunt maiores vero. Sed natus nihil facere!</p>
+            <ul className='flex mb-5 text-white'>
                 <li className='border border-primary rounded-md p-2'>NextJS </li>
                 <li className='border border-primary rounded-md p-2'>Tailwind </li>
                 <li className='border border-primary rounded-md p-2'>JavaScript </li>
@@ -63,14 +63,14 @@ const Work = () => {
         <div className='grid grid-cols-12'>
           <div className='row-span-full col-start-2 md:col-start-6 col-end-12 md:col-end-13 col-span-7 flex flex-col justify-center gap-2 py-2 z-10'>
             <h3 className='md:text-end text-sm font-semibold text-primary'>Featured Project #1</h3>
-            <h2 className='text-2xl md:text-4xl font-bold md:text-end'>Pro-Lab</h2>
-            <p className='md:text-lg rounded-md   md:bg-gray-400 py-2 px-1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eum odit impedit autem quo quisquam quidem dolorum, temporibus enim similique nemo sapiente ratione nesciunt maiores vero. Sed natus nihil facere!</p>
-            <ul className='flex md:justify-end gap-2 mb-5'>
+            <h2 className='text-2xl md:text-4xl text-white font-bold md:text-end'>Pro-Lab</h2>
+            <p className='md:text-lg rounded-md text-white md:text-end md:bg-gray-400 py-2 px-1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eum odit impedit autem quo quisquam quidem dolorum, temporibus enim similique nemo sapiente ratione nesciunt maiores vero. Sed natus nihil facere!</p>
+            <ul className='flex md:justify-end  text-white gap-2 mb-5'>
                 <li className='border border-primary rounded-md p-2'>NextJS </li>
                 <li className='border border-primary rounded-md p-2'>Tailwind </li>
                 <li className='border border-primary rounded-md p-2'>JavaScript </li>
             </ul>
-            <div className='flex md:justify-end gap-2'>
+            <div className='flex md:justify-end  gap-2'>
                 <Link href="/"><BsGithub size={35} className='bg-white text-black rounded-md p-1 hover:bg-primary hover:scale-110 duration-500'/> </Link>
                 <Link href="/"> <ImArrowUpRight size={35} className='bg-white text-black p-2 rounded-md hover:bg-primary hover:scale-110 duration-500'/> </Link>
             </div>
