5fcc046a218ed8c1f89fea165d99f7b3e50a9c54
shalom99 <castroshalom77@gmail.com>
2023-05-31 21:32:40 +0800

    removed resume in social links

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -71,9 +71,9 @@ export default function Header() {
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#45a29e]">
                 <a className="flex justify-between items-center w-full text-gray-300" href="mailto:castroshalom77@gmail.com">Email  <HiOutlineMail size={30}/> </a>
             </li>
-            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            {/* <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
                 <a className="flex justify-between items-center w-full text-gray-300" href="/">Resume  <BsFillPersonLinesFill size={30}/> </a>
-            </li>
+            </li> */}
         </ul>
       </div>
     </div>
